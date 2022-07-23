7fb388f6176ff6f9410e9c10dc3de967660d9983
施颖杰 <1577310596@qq.com>
2022-07-23 22:33:33 +0800

    chore: add vitest-coverage

--- src/__test__/add.spec.ts
+++ src/__test__/add.spec.ts
@@ -1,7 +1,9 @@
 import { describe, expect, it } from 'vitest'
 import useAdd from './../hooks/useAdd';
-describe('suite', () => {
+describe('first test', () => {
   it('1 + 1 equals should be 2', () => {
     expect(useAdd(1, 1)).toBe(2)
+    expect(useAdd(2, 1)).toBe(3)
+    expect(useAdd(1, 1.1)).toBe(2.1)
   });
 })
